8f6e660aa01cc89e6c197d702ef5752646ab4e1f
nxnmai <nxnmaivv@gmail.com>
2025-06-30 17:12:13 +0700

    upd

--- Projects/SwinAdventure/SwinAdventure.Tests/obj/Debug/net8.0/SwinAdventure.Tests.AssemblyInfo.cs
+++ Projects/SwinAdventure/SwinAdventure.Tests/obj/Debug/net8.0/SwinAdventure.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwinAdventure.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33174ddf01908c50b920afbf07d062b1b444f96")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwinAdventure.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwinAdventure.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
